e9c035af1b0bdaf97f54de82c21392667edcfca4
meddeb23 <dalimeddeb132@gmial.com>
2021-04-07 00:23:59 +0100

    add searching feature

--- server/routes/api/trip.js
+++ server/routes/api/trip.js
@@ -3,30 +3,54 @@ const Joi = require("joi");
 const router = express.Router();
 const { Trip } = require("../../models/Trips");
 const { Station } = require("../../models/Station");
+
+const _ = require("lodash");
 const admin = require("../../middleware/admin");
-const { all } = require("./auth");
+const debug = require("debug")("app:routes");
+
+const validateTripSearch = (query) => {
+  const schema = {
+    origin: Joi.string().min(5).max(50).required(),
+    destination: Joi.string().min(5).max(50).required(),
+    type_of_transport: Joi.string()
+      .min(3)
+      .max(10)
+      .required()
+      .allow("train", "bus"),
+  };
+
+  return Joi.validate(query, schema);
+};
+
+// @route   GET api/v1/trip
+// @desc    Get trip
+// @access  admin
+router.get("/search", async (req, res) => {
+  debug(req.query);
+  const { error } = validateTripSearch(req.query);
+  if (error) return res.status(400).json(error.details[0].message);
+  const { origin, destination, type_of_transport } = req.query;
+
+  const originTrip = await Station.findOne({ name: origin });
+  if (!originTrip) return res.status(400).json({ message: "invalid origin" });
+  const destinationTrip = await Station.findOne({ name: destination });
+  if (!destinationTrip)
+    return res.status(400).json({ message: "invalid destination" });
+
+  const all_trips = await Trip.find({
+    station: [originTrip._id, destinationTrip._id],
+    type_of_transport,
+  }).populate("station");
+
+  res.json(all_trips);
+});
 
 // @route   GET api/v1/trip
 // @desc    Get trip
 // @access  admin
 router.get("/", admin, async (req, res) => {
-  const all_trips = await Trip.find();
-
-  let result = [];
-
-  for (let trip of all_trips) {
-    const full_station = trip.station.map(async (i) => {
-      const t = await Station.findById(i);
-      console.log(t);
-      return t;
-    });
-    console.log(full_station);
-    trip.station = full_station;
-    console.log(trip, full_station);
-    result.push(trip);
-  }
-
-  res.json(result);
+  const all_trips = await Trip.find().populate("station");
+  res.json(all_trips);
 });
 
 // @route   POST api/v1/trip
@@ -34,7 +58,7 @@ router.get("/", admin, async (req, res) => {
 // @access  admin
 router.post("/", admin, async (req, res) => {
   const { error } = validate(req.body);
-  if (error) return res.status(400).json(error);
+  if (error) return res.status(400).json(error.details[0].message);
   const newTrip = new Trip(req.body);
   const savedTrip = await newTrip.save();
   return res.json({ trip: savedTrip });
@@ -45,7 +69,12 @@ router.post("/", admin, async (req, res) => {
 // @access  admin
 router.patch("/update/:id", admin, async (req, res) => {
   const { id } = req.params;
-  res.send(`update trip with id = ${id} 😄`);
+  const update_values = req.body;
+  const { error } = validate_update(update_values);
+  if (error) return res.status(400).json(error.details[0].message);
+  debug(update_values);
+  const newTrip = await Trip.findByIdAndUpdate(id, update_values);
+  res.json({ message: "trip updated", success: true });
 });
 
 // @route   DELETE api/v1/trip
@@ -53,8 +82,10 @@ router.patch("/update/:id", admin, async (req, res) => {
 // @access  admin
 router.delete("/delete", admin, async (req, res) => {
   const { trip_list } = req.body;
-  console.log(trip_list);
-  res.send("delete a trip 😄");
+  const l = await Trip.deleteMany({ _id: { $in: trip_list } });
+  debug(l);
+  if (l.deletedCount === 0) return res.json({ success: false });
+  return res.json({ message: "trips deleted", success: true });
 });
 
 // @route   GET api/v1/trip
@@ -62,7 +93,8 @@ router.delete("/delete", admin, async (req, res) => {
 // @access  admin
 router.get("/:id", admin, async (req, res) => {
   const { id } = req.params;
-  res.send(`get trip with id = ${id} 😄`);
+  const trip = await Trip.findById(id).populate("station");
+  res.json({ trip });
 });
 
 const validate = (req) => {
@@ -73,6 +105,15 @@ const validate = (req) => {
     arrival_time: Joi.date().required(),
     distance: Joi.number().greater(0).required(),
     type_of_transport: Joi.allow("train", "bus"),
+  };
+  return Joi.validate(req, schema);
+};
+
+const validate_update = (req) => {
+  const schema = {
+    price: Joi.number().greater(0),
+    depart_time: Joi.date(),
+    arrival_time: Joi.date(),
   };
   return Joi.validate(req, schema);
 };
